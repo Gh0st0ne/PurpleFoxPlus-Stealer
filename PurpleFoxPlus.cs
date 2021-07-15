76d5c01b904e49551ca65c9fbdb816486270d0d6
Exploit Blizzard <61627070+exploitblizzard@users.noreply.github.com>
2021-07-16 02:41:08 +0530

    Create PurpleFoxPlus.cs

---
+++ PurpleFoxPlus.cs
@@ -0,0 +1,14 @@
+using System;
+  
+namespace HelloWorldApp {
+     
+    class Geeks {
+          
+        static void Main(string[] args) {
+              
+            // printing Hello World!
+            Console.WriteLine("Hello, This is Purple Fox Plus Stealer!");
+            Console.ReadKey();
+        }
+    }
+}
